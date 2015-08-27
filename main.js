e5b2b9c949c1dd96d83b7bdfbc3a79fda497a645
hu9o <hugo.ratiney@gmail.com>
2015-08-27 13:25:04 +0400

    Fix whitespace

--- main.js
+++ main.js
@@ -72,18 +72,18 @@ define(function (require, exports, module) {
         }
 
         var _proxyProtocol = getProxyProtocol(_proxyString);
-		/* Code Inspection */
-		$('#prefUI-cI').prop('checked', (PreferencesManager.get("linting.enabled") == true) ? true : false);
-		/* useTabChar */
-		$('#prefUI-tabChar').prop('checked', (PreferencesManager.get("useTabChar") == true) ? true : false);
-		/* tabSize */
-		$("#prefUI-tabSize").val(PreferencesManager.get("tabSize"));
-		/* spaceUnits */
-		$("#prefUI-spaceUnit").val(PreferencesManager.get("spaceUnits"));
-		/* wordWrap */
-		$('#prefUI-wordWrap').prop('checked', (PreferencesManager.get("wordWrap") == true) ? true : false);
-		/* proxy */
-		$('#prefUI-proxyEnabled').prop('checked', (prefs.get("proxy-enabled") == true) ? true : false);
+        /* Code Inspection */
+        $('#prefUI-cI').prop('checked', (PreferencesManager.get("linting.enabled") == true) ? true : false);
+        /* useTabChar */
+        $('#prefUI-tabChar').prop('checked', (PreferencesManager.get("useTabChar") == true) ? true : false);
+        /* tabSize */
+        $("#prefUI-tabSize").val(PreferencesManager.get("tabSize"));
+        /* spaceUnits */
+        $("#prefUI-spaceUnit").val(PreferencesManager.get("spaceUnits"));
+        /* wordWrap */
+        $('#prefUI-wordWrap').prop('checked', (PreferencesManager.get("wordWrap") == true) ? true : false);
+        /* proxy */
+        $('#prefUI-proxyEnabled').prop('checked', (prefs.get("proxy-enabled") == true) ? true : false);
         $('input[name=prefUI-proxyProtocol][value='+_proxyProtocol+']').attr('checked', true);
 
         if(_proxyString != undefined && _proxyString != null) {
@@ -94,18 +94,18 @@ define(function (require, exports, module) {
         }
         $("#prefUI-proxy").val(PreferencesManager.get("proxy"));
         /* smartIndent */
-		$('#prefUI-smartIndent').prop('checked', (PreferencesManager.get("smartIndent") == true) ? true : false);
-		/* closeTags */
-		/* insertHintOnTab */
-		$('#prefUI-insertHint').prop('checked', (PreferencesManager.get("insertHintOnTab") == true) ? true : false);
-		/* sortDirectoriesFirst */
-		$('#prefUI-sortDir').prop('checked', (PreferencesManager.get("sortDirectoriesFirst") == true) ? true : false);
-		/* staticserver.port */
-		$("#prefUI-serverPort").val(PreferencesManager.get("staticserver.port"));
-		/* scrollPastEnd */
-		$('#prefUI-scrollPastEnd').prop('checked', (PreferencesManager.get("scrollPastEnd") == true) ? true : false);
-		/* softTabs */
-		$('#prefUI-softTabs').prop('checked', (PreferencesManager.get("softTabs") == true) ? true : false);
+        $('#prefUI-smartIndent').prop('checked', (PreferencesManager.get("smartIndent") == true) ? true : false);
+        /* closeTags */
+        /* insertHintOnTab */
+        $('#prefUI-insertHint').prop('checked', (PreferencesManager.get("insertHintOnTab") == true) ? true : false);
+        /* sortDirectoriesFirst */
+        $('#prefUI-sortDir').prop('checked', (PreferencesManager.get("sortDirectoriesFirst") == true) ? true : false);
+        /* staticserver.port */
+        $("#prefUI-serverPort").val(PreferencesManager.get("staticserver.port"));
+        /* scrollPastEnd */
+        $('#prefUI-scrollPastEnd').prop('checked', (PreferencesManager.get("scrollPastEnd") == true) ? true : false);
+        /* softTabs */
+        $('#prefUI-softTabs').prop('checked', (PreferencesManager.get("softTabs") == true) ? true : false);
         /* closeBrackets */
         $('#prefUI-closeBrackets').prop('checked', (PreferencesManager.get("closeBrackets") == true) ? true : false);
         /* dragDropText */
@@ -160,7 +160,7 @@ define(function (require, exports, module) {
         /* Set current values */
         loadPreferences();
 
-		$("#prefUISubmit").on("click", function (e) {
+    $("#prefUISubmit").on("click", function (e) {
             /* jslint.options */
             /* Code Inspection */
             PreferencesManager.set("linting.enabled", ($('#prefUI-cI').is(':checked')) ? true : false);
